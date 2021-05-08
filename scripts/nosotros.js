d1f317933ebd583b5259e1a66380bec974198d8d
Tomas Balducchi <tomasbalducchi@gmail.com>
2021-05-08 11:17:02 -0300

    añadida funcionalidad dark mode en nosotros y cambio de fondo de hero

--- scripts/nosotros.js
+++ scripts/nosotros.js
@@ -1,1 +1,13 @@
+const brightMode = document.getElementById("brightMode");
+const darkMode = document.getElementById("darkMode");
 
+darkMode.addEventListener("click", () => {
+  nosotros.classList.remove("showed");
+  nosotros.classList.add("hidden");
+  nosotrosb.classList.add("showed");
+});
+
+brightMode.addEventListener("click", () => {
+  nosotrosb.classList.remove("showed");
+  nosotros.classList.add("showed");
+});
